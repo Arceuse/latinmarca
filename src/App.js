1775832db716bbc1e442f17c702ff1dea4cef1d4
ethan arredondo <ethansarredondo@gmail.com>
2024-08-12 11:49:35 -0500

    Diseño de Navbar terminado
    falta agregar sus enlaces correspondientes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Navbar />
       <Routes>
         <Route path="/" element={<Inicio />} />
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,47 +1,29 @@
 import React from "react";
+import './navbar.css'
 
 const Navbar = () => {
     return (
         <div>
             <div>
-                <div className="navbar">
-                    <header class="p-3 mb-3 border-bottom">
-                        <div class="container">
-                            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
-                                </a>
-
-                                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                                    <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li>
-                                    <li><a href="#" class="nav-link px-2 link-body-emphasis">Inventory</a></li>
-                                    <li><a href="#" class="nav-link px-2 link-body-emphasis">Customers</a></li>
-                                    <li><a href="#" class="nav-link px-2 link-body-emphasis">Products</a></li>
+                <header className="p-3 mb-3 border-bottom navbar-diseño">
+                    <div className="container">
+                        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                            <div className="px-3">
+                                <h1 className="marca">LatinMarca</h1>
+                            </div>
+                            <div className="opciones-navbar">
+                                <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                                    <li><a href="#" className="nav-link px-2 opcion">Inicio</a></li>
+                                    <li><a href="#" className="nav-link px-2 opcion">Servicios</a></li>
+                                    <li><a href="#" className="nav-link px-2 opcion">Productos</a></li>
+                                    <li><a href="#" className="nav-link px-2 opcion">Blog</a></li>
+                                    <li><a href="#" className="nav-link px-2 opcion">Local</a></li>
+                                    <li><a href="#" className="nav-link px-2 opcion">Contactenos</a></li>
                                 </ul>
-
-                                <div class="dropdown text-end">
-                                    <ul class="dropdown-menu text-small">
-                                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                                        <li><hr class="dropdown-divider"/></li>
-                                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                                    </ul>
-                                </div>
                             </div>
                         </div>
-                    </header>
-                    <div>
-                        <h1>LatinMarca</h1>
-                        <ul>
-                            <li>Inicio</li>
-                            <li>Servicios</li>
-                            <li>Productos</li>
-                            <li>Blog</li>
-                            <li>Local</li>
-                            <li>Contactenos</li>
-                        </ul>
                     </div>
-                </div>
+                </header>
             </div>
         </div>
     )
