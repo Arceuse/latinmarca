ae1158461fab553db8f9ad7817a8e86011f53d73
ethan arredondo <ethansarredondo@gmail.com>
2024-08-14 00:09:28 -0500

    Finalización de diseño de seccion 1 y 2
    seccion 1 y 2 creadas correctamente

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -12,14 +12,45 @@ const Inicio = () => {
                     <h1>BRINDAMOS SERVICIOS A DOMICILIO EN TODO LIMA</h1>
                     <p>Realizamos servicios de instalaciones, mantenimiento y asesorias</p>
                     <div className="boton-sabermas">
-                        <a href='#'>Saber más</a>
+                        <a href='#'>Saber más ➜ </a>
                     </div>
                 </div>
             </div>
             <div className="seccion2">
-
+                <div className='texto'>
+                    <h1>ASESORIA Y FACILIDAD EN LA COMPRA DE EQUIPOS</h1>
+                    <p>Ayudamos a la compra de equipos y accesorios aconsejando lo más óptimo para sus necesidades</p>
+                    <div className="boton-sabermas">
+                        <a href='#'>Saber más ➜ </a>
+                    </div>
+                </div>
+                <div className='imagen'>
+                    <img src="./img/compra2.png"></img>
+                </div>
             </div>
             <div className="seccion3">
+                <div className='imagen'>
+                    <img src="./img/reparacion.png"></img>
+                </div>
+                <div className='texto'>
+                    <h1>BRINDAMOS SERVICIOS A DOMICILIO EN TODO LIMA</h1>
+                    <p>Realizamos servicios de instalaciones, mantenimiento y asesorias</p>
+                    <div className="boton-sabermas">
+                        <a href='#'>Saber más ➜ </a>
+                    </div>
+                </div>
+            </div>
+            <div className="seccion4">
+                <div className='imagen'>
+                    <img src="./img/reparacion.png"></img>
+                </div>
+                <div className='texto'>
+                    <h1>BRINDAMOS SERVICIOS A DOMICILIO EN TODO LIMA</h1>
+                    <p>Realizamos servicios de instalaciones, mantenimiento y asesorias</p>
+                    <div className="boton-sabermas">
+                        <a href='#'>Saber más ➜ </a>
+                    </div>
+                </div>
             </div>
         </div>
     );
